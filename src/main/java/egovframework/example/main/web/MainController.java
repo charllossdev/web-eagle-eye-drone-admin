3a1d94eac7011201d38fec1a4f9a49ca200120d1
charllossDev <charlloss.dev@gmail.com>
2019-03-04 01:43:24 +0900

    source: Update Url Setting - Init Start Addr is Home

--- src/main/java/egovframework/example/main/web/MainController.java
+++ src/main/java/egovframework/example/main/web/MainController.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	@RequestMapping(value = "/main.do")
+	@RequestMapping(value = "/home.do")
+	public String initHome() throws Exception {
+		return "main/main.adminTiles";
+	}
+	
+	@RequestMapping(value = "/Main.do")
 	public String initMain() throws Exception {
 		return "main/main.adminTiles";
+	}
+	
+	@RequestMapping(value = "/UserGrade.do")
+	public String initUserGrade() throws Exception {
+		
+		return "main/UserGrade";
 	}
 }
